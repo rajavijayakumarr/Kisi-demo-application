e53c5a8ce8364fdb86bca9ab13c55af70df2ec31
Your Name <you@example.com>
2018-09-26 15:19:45 +0530

    Initial Commit

---
+++ Kisi demo applicationTests/Kisi_demo_applicationTests.swift
@@ -0,0 +1,34 @@
+//
+//  Kisi_demo_applicationTests.swift
+//  Kisi demo applicationTests
+//
+//  Created by Raja on 26/09/18.
+//  Copyright © 2018 FullCreative. All rights reserved.
+//
+
+import XCTest
+@testable import Kisi_demo_application
+
+class Kisi_demo_applicationTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
