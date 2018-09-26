793acfde7740ff8f34ad3c9e55907ce1b8d81b70
Your Name <you@example.com>
2018-09-26 15:38:10 +0530

    got the successful response from the server now can access the authorisation token next going to implement the unlocking of the door

---
+++ Kisi demo application/OpenDoorViewController.swift
@@ -0,0 +1,30 @@
+//
+//  OpenDoorViewController.swift
+//  Kisi demo application
+//
+//  Created by Raja on 26/09/18.
+//  Copyright © 2018 FullCreative. All rights reserved.
+//
+
+import UIKit
+
+class OpenDoorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Kisi demo application/ViewController.swift
+++ Kisi demo application/ViewController.swift
@@ -8,13 +8,48 @@
 
 import UIKit
 
+let LOGIN_ENDPOINT = "https://api.getkisi.com/users/sign_in"
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
     }
 
+    @IBAction func loginButtonAction(_ sender: Any) {
+        
+        let url = URL(string: LOGIN_ENDPOINT)!
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        
+        request.httpBody = "{\"user\": {\"email\": \"\(self.emailTextField.text ?? "no email")\",\"password\": \"\(self.passwordTextField.text ?? "no password")\"}}".data(using: .utf8)
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            if let response = response, let data = data {
+                print(response)
+                print(String(data: data, encoding: .utf8) as Any)
+            } else {
+                print(error as Any)
+            }
+        }
+        
+        task.resume()
+    }
+    
+}
 
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return true
+    }
 }
 
